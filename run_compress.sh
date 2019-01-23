37c231e6417fc3a01cc212d56594aa71c59501b4
BJW-2-13440 <zhengliu@microsoft.com>
2019-01-23 20:14:55 +0800

    compress-mean

--- run_compress.sh
+++ run_compress.sh
@@ -2,4 +2,6 @@ python main.py models="lz-compress-plus-3","lz-compress-plus-5","lz-compress-plu
 
 python main.py models="lz-vanilla-compress-3","lz-vanilla-compress-5","lz-vanilla-compress-10" rounds=3 epochs=3
 
+python main.py models="lz-compress-plus-mean-3","lz-compress-plus-mean-5","lz-compress-plus-mean-10" rounds=3 epochs=3
+
 # python main.py models="lz-base" rounds=1 epochs=3
