616685322cdc40a52bd0c0cd9b544d25a61af41e
BJW-2-13440 <zhengliu@microsoft.com>
2019-01-23 14:22:00 +0800

    .sh

---
+++ run_compress.sh
@@ -0,0 +1,5 @@
+python main.py models="lz-compress-plus-3","lz-compress-plus-5","lz-compress-plus-10" rounds=3 epochs=3
+
+python main.py models="lz-vanilla-compress-3","lz-vanilla-compress-5","lz-vanilla-compress-10" rounds=3 epochs=3
+
+# python main.py models="lz-base" rounds=1 epochs=3
